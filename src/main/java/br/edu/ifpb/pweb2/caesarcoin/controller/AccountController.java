c5568d1ca36a3d6fa158ccd1f72de3947d3fbebe
Bruno <brunovn.eneas@gmail.com>
2025-06-01 11:38:10 -0300

    feat: account controller finished

--- src/main/java/br/edu/ifpb/pweb2/caesarcoin/controller/AccountController.java
+++ src/main/java/br/edu/ifpb/pweb2/caesarcoin/controller/AccountController.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.edu.ifpb.pweb2.caesarcoin.model.Account;
 import br.edu.ifpb.pweb2.caesarcoin.model.AccountOwner;
@@ -37,4 +40,19 @@ public class AccountController {
         return accOwnerService.findAll();
     }
 
+
+    @PostMapping
+    public ModelAndView save(Account account, ModelAndView model, RedirectAttributes attr){
+        accService.save(account);
+        attr.addFlashAttribute("mensage","Conta inserida com sucesso");
+        model.setViewName("redirect:/accounts");
+        return model;
+    }
+
+    @GetMapping("/{id}")
+    public ModelAndView getAccOwnerById(@PathVariable(value = "id") Integer id, ModelAndView model){
+        model.addObject("account", accService.findById(id));
+        model.setViewName("accounts/form");
+        return model;
+    }
 }
