c6a52b09dfac4386e4551e1c8eca244fa93e76f0
Bruno <brunovn.eneas@gmail.com>
2025-06-01 10:57:11 -0300

    feat: created backend logic for accounts

--- src/main/java/br/edu/ifpb/pweb2/caesarcoin/controller/AccountController.java
+++ src/main/java/br/edu/ifpb/pweb2/caesarcoin/controller/AccountController.java
@@ -1,5 +1,40 @@
 package br.edu.ifpb.pweb2.caesarcoin.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.edu.ifpb.pweb2.caesarcoin.model.Account;
+import br.edu.ifpb.pweb2.caesarcoin.model.AccountOwner;
+import br.edu.ifpb.pweb2.caesarcoin.service.AccountOwnerService;
+import br.edu.ifpb.pweb2.caesarcoin.service.AccountService;
+
+@Controller
+@RequestMapping("/accounts")
 public class AccountController {
     
+    @Autowired
+    private AccountService accService;
+
+    @Autowired
+    private AccountOwnerService accOwnerService;
+
+    @GetMapping("/form")
+    public ModelAndView getForm(ModelAndView model) {
+        model.setViewName("accounts/form");
+        model.addObject("account", new Account(new AccountOwner()));
+        return model;
+    }
+
+
+    @ModelAttribute("accountOwnerItens")
+    public List<AccountOwner> getAccountOwner(){
+        return accOwnerService.findAll();
+    }
+
 }
--- src/main/java/br/edu/ifpb/pweb2/caesarcoin/model/Account.java
+++ src/main/java/br/edu/ifpb/pweb2/caesarcoin/model/Account.java
@@ -1,13 +1,27 @@
 package br.edu.ifpb.pweb2.caesarcoin.model;
 
+import java.io.Serializable;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.Data;
 
 @Data
-public class Account {
+public class Account implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     public Integer id;
     public String number;
     public String description;
     public String type;
+    
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
     public Integer dueDate;
-    public Integer accountOwner;
+
+    public AccountOwner accountOwner;
+
+    public Account(AccountOwner accOwner){
+        this.accountOwner = accOwner;
+    }
+    
 }
