a6aaef56c2609b9bb2120e152cecdba3acfc9221
Douglas Carneiro <douglas.ul@gmail.com>
2025-06-08 15:07:15 -0300

    fix: adjusting account dueDate

--- src/main/java/br/edu/ifpb/pweb2/caesarcoin/model/Account.java
+++ src/main/java/br/edu/ifpb/pweb2/caesarcoin/model/Account.java
@@ -1,6 +1,7 @@
 package br.edu.ifpb.pweb2.caesarcoin.model;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -15,8 +16,8 @@ public class Account implements Serializable {
     public String description;
     public String type;
     
-    @DateTimeFormat(pattern = "dd/MM/yyyy")
-    public String dueDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    public Date dueDate;
 
     public AccountOwner accountOwner;
 
