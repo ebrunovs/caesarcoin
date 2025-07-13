75b4d35697fc2f364faf50220340bc2fdf1d7a27
Bruno Eneas <brunovn.eneas@gmail.com>
2025-07-13 17:50:28 -0300

    style: homepage styling

--- src/main/java/br/edu/ifpb/pweb2/caesarcoin/controller/HomeController.java
+++ src/main/java/br/edu/ifpb/pweb2/caesarcoin/controller/HomeController.java
@@ -2,11 +2,15 @@ package br.edu.ifpb.pweb2.caesarcoin.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
+    
     @RequestMapping("/home")
-    public String home() {
-        return "index";
+    public ModelAndView home(ModelAndView model) {
+        model.addObject("menu", "home");
+        model.setViewName("index");
+        return model;
     }
 }
