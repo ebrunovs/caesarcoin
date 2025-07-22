3513cd471a3351ee972239de3d5992a95a0e4e84
Douglas Carneiro <douglas.ul@gmail.com>
2025-07-21 22:54:36 -0300

    fix: update date format in Transaction model

--- src/main/java/br/edu/ifpb/pweb2/caesarcoin/model/Transaction.java
+++ src/main/java/br/edu/ifpb/pweb2/caesarcoin/model/Transaction.java
@@ -21,7 +21,7 @@ public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate date;
     private String description;
     private Double value;
