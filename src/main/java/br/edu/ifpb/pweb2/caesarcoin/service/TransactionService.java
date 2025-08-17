6f1f00a70e87940c4adcdd474cb279a3aa3bd39c
Douglas Carneiro <douglas.ul@gmail.com>
2025-08-17 17:20:37 -0300

    feat: add account extract feature with transaction summary and filtering options

--- src/main/java/br/edu/ifpb/pweb2/caesarcoin/service/TransactionService.java
+++ src/main/java/br/edu/ifpb/pweb2/caesarcoin/service/TransactionService.java
@@ -1,10 +1,13 @@
 package br.edu.ifpb.pweb2.caesarcoin.service;
 
+import br.edu.ifpb.pweb2.caesarcoin.model.Account;
+import br.edu.ifpb.pweb2.caesarcoin.model.ExtractData;
 import br.edu.ifpb.pweb2.caesarcoin.model.Transaction;
 import br.edu.ifpb.pweb2.caesarcoin.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Component
@@ -31,6 +34,52 @@ public class TransactionService implements Service<Transaction, Integer> {
     @Override
     public Transaction save(Transaction transaction) {
         return transactionRepository.save(transaction);
+    }
+
+    public List<Transaction> findByAccountAndDateBetween(Account account, LocalDate startDate, LocalDate endDate) {
+        return transactionRepository.findByAccountAndDateBetween(account, startDate, endDate);
+    }
+
+    public ExtractData generateExtract(Account account, LocalDate startDate, LocalDate endDate) {
+        List<Transaction> transactions = findByAccountAndDateBetween(account, startDate, endDate);
+        
+        double totalIncomes = transactions.stream()
+            .filter(t -> "ENTRADA".equals(t.getType().toString()))
+            .mapToDouble(Transaction::getValue)
+            .sum();
+            
+        double totalOutcomes = transactions.stream()
+            .filter(t -> "SAIDA".equals(t.getType().toString()))
+            .mapToDouble(Transaction::getValue)
+            .sum();
+            
+        double totalInvestments = transactions.stream()
+            .filter(t -> "INVESTIMENTO".equals(t.getType().toString()))
+            .mapToDouble(Transaction::getValue)
+            .sum();
+            
+        double periodBalance = totalIncomes - totalOutcomes;
+
+        return new ExtractData(transactions, startDate, endDate, totalIncomes, totalOutcomes, totalInvestments, periodBalance);
+    }
+
+    public ExtractData generateExtractWithDefaultDates(Account account, String startDateStr, String endDateStr) {
+        LocalDate startDate;
+        LocalDate endDate;
+        
+        if (startDateStr != null && !startDateStr.isEmpty()) {
+            startDate = LocalDate.parse(startDateStr);
+        } else {
+            startDate = LocalDate.now().withDayOfMonth(1);
+        }
+        
+        if (endDateStr != null && !endDateStr.isEmpty()) {
+            endDate = LocalDate.parse(endDateStr);
+        } else {
+            endDate = LocalDate.now();
+        }
+
+        return generateExtract(account, startDate, endDate);
     }
 
 
