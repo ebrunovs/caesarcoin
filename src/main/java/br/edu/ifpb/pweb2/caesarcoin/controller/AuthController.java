a024d8b920e44e2e5742fa0e9fd7173693135cdf
Bruno Eneas <brunovn.eneas@gmail.com>
2025-08-25 23:13:34 -0300

    refactor: remove unused imports in AuthController

--- src/main/java/br/edu/ifpb/pweb2/caesarcoin/controller/AuthController.java
+++ src/main/java/br/edu/ifpb/pweb2/caesarcoin/controller/AuthController.java
@@ -1,13 +1,10 @@
 package br.edu.ifpb.pweb2.caesarcoin.controller;
 
-import javax.naming.Binding;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
